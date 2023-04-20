318c449e01ae78d47a624e0482de431ec1e7512f
basementlivin <epedersen13@gmail.com>
2023-04-20 13:06:37 -0400

    Home stretch push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,13 @@ export default function Footer () {
     return (
         <div className="footer">
             <EqualHousing></EqualHousing>
-            <p className="footer__copy h3__sans">info@theellery.com</p>
-            <div className="footer__address">
-                <p className="footer__copy h3__sans">294 Amarillo Road<br></br>Grand Prairie, Texas 293821</p>
+            <div className="footer__copy">
+                <p className="footer__copy__email h3__sans">info@theellery.com</p>
+                <div className="footer__address">
+                    <p className="footer__copy__address h3__sans">294 Amarillo Road<br></br>Grand Prairie, Texas 293821</p>
+                </div>
+                <p className="footer__copy__phone h3__sans">800-009-0009</p>
             </div>
-            <p className="footer__copy h3__sans">800-009-0009</p>
             <Accessibility></Accessibility>
         </div>
     )
