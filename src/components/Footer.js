0036263f20910cc3d393e6622f4a5ca528de7a2e
basementlivin <epedersen13@gmail.com>
2023-04-20 10:01:54 -0400

    Adjust header/footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,17 @@ import { ReactComponent as Accessibility } from "../assets/graphics/ACCESSIBILIT
 export default function Footer () {
     return (
         <div className="footer">
-            <div className="footer__graphic"><EqualHousing></EqualHousing></div>
+            <div className="footer__graphic">
+                <EqualHousing></EqualHousing>
+            </div>
             <p className="footer__copy">info@theellery.com</p>
             <div className="footer__address">
-                <p className="footer__copy">294 Amarillo Road</p>
-                <p className="footer__copy">Grand Prairie, Texas 293821</p>
+                <p className="footer__copy">294 Amarillo Road<br></br>Grand Prairie, Texas 293821</p>
             </div>
             <p className="footer__copy">800-009-0009</p>
-            <div className="footer__graphic"><Accessibility></Accessibility></div>
+            <div className="footer__graphic">
+                <Accessibility></Accessibility>
+            </div>
         </div>
     )
 }
