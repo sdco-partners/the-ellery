5d4642e4aaf21b5aa08372a25ead3d3b84cf46ae
basementlivin <epedersen13@gmail.com>
2023-04-20 16:34:37 -0400

    Fade-ins and general finesse

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,14 +2,21 @@ import React from "react";
 import Header from "../components/Header";
 import Content from "../components/CenteredContent";
 import Footer from "../components/Footer";
+import FadeIn from "react-fade-in/lib/FadeIn";
 
 export default function Landing () {
     return (
         <>
             <div className="landing wrapper">
-                <Header></Header>
-                <Content></Content>
-                <Footer></Footer>
+                <FadeIn delay={500} transitionDuration={800}>
+                    <Header></Header>
+                </FadeIn>
+                <FadeIn delay={1200} transitionDuration={1800}>
+                    <Content></Content>
+                </FadeIn>
+                <FadeIn delay={700} transitionDuration={1000}>
+                    <Footer></Footer>
+                </FadeIn>
             </div>
         </>
     )
