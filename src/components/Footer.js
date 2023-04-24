f62dba3fff657632982acd795cc4b07d1564d7c6
basementlivin <epedersen13@gmail.com>
2023-04-24 15:15:20 -0400

    Update address and phone number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ export default function Footer () {
             <div className="footer__copy">
                 <p className="footer__copy__email h3__sans">info@theellery.com</p>
                 <div className="footer__address">
-                    <p className="footer__copy__address h3__sans">294 Amarillo Road<br></br>Grand Prairie, Texas 293821</p>
+                    <p className="footer__copy__address h3__sans">4210 Dechman Drive<br></br>Grand Prairie, Texas 75052</p>
                 </div>
-                <p className="footer__copy__phone h3__sans">800-009-0009</p>
+                <p className="footer__copy__phone h3__sans">469.361.4424</p>
             </div>
             <Accessibility></Accessibility>
         </div>
